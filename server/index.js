8804403850899120db1449378968678c4ee5f486
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-09-11 18:33:08 +0530

    added api

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import googleAuthConfig from "./config/google.config";
 
 // microservice routes
 import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+import Food from "./API/Food";
 
 // database connection
 import ConnectDB from "./database/connection";
@@ -31,7 +33,11 @@ googleAuthConfig(passport);
 
 
 // appliction routes
-zomato.use("/auth",Auth);
+zomato.use("/auth", Auth);
+zomato.use ("/restaurant", Restaurant);
+zomato.use("/food", Food);
+
+
 
 zomato.get("/", (req, res) => res.json({ message: "setup success"}));
 
