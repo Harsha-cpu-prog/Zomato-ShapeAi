722def518fd6d364b0a87d812979ac93dd68bfc4
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-09-11 17:43:29 +0530

    added google

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import passport from "passport";
 
 // Models
 import { UserModel } from "../../database/user";
@@ -18,6 +19,7 @@ Method    POST
 Router.post("/signup", async (req,res)=>{
     try{
         await UserModel.findByEmailAndPhone(req.body.credentials);
+      
         //  save DB
        const newUser= await UserModel.create(req.body.credentials);
         // generate jwt token
@@ -25,7 +27,6 @@ Router.post("/signup", async (req,res)=>{
         // return
         return res.status(200).json({ token , status:"success"});
     }catch (error) {
-       console.log(error);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -43,10 +44,41 @@ const user = await UserModel.findByEmailAndPassword(req.body.credentials);
   const token = user.generateJwtToken();
  return res.status(200).json({ token , status:"success"});
  }catch (error) {
-       console.log(error);
         return res.status(500).json({ error: error.message });
     }
 });
+
+/*
+Route     /google
+Des       Google Signin
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+    "/google",
+    passport.authenticate("google", {
+      scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+      ],
+    })
+  );
+
+/*
+Route     /google/callback
+Des       Google Signin Callback
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+    "/google/callback",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+      return res.json({ token: req.session.passport.user.token });
+    }
+  );
 
 
 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
 
+// configs
+import googleAuthConfig from "./config/google.config";
 
 // microservice routes
 import Auth from "./API/Auth";
@@ -16,10 +19,16 @@ import ConnectDB from "./database/connection";
 const zomato = express();
  
 // application middlewares
-zomato.use(express());
+zomato.use(express.json());
 zomato.use(express.urlencoded({extended:false}));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport confi
+googleAuthConfig(passport);
+
 
 // appliction routes
 zomato.use("/auth",Auth);
